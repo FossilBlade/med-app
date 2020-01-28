ddd62f8412b44452f49d789e6ed3d183fef1f157
FB <r@g.com>
2020-01-28 15:08:10 +1300

    added install

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@ sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo apt -y update && sudo apt-get install -y python3 python3-dev python3-venv python3-pip python-certbot-nginx build-essential
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io
+
+sudo systemctl start docker && sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
 
 
 rm -rf /appl/demo-app-api
-rm -rf /etc/systemd/system/api.service /etc/systemd/system/celery.service
+sudo rm -rf /etc/systemd/system/api.service /etc/systemd/system/celery.service
 sudo mkdir -p /appl/demo-app-api
 sudo chown -R ubuntu:ubuntu /appl
 cp -r api/* /appl/demo-app-api/
@@ -15,11 +20,14 @@ python3 -m venv /appl/demo-app-api/venv
 sudo cp api.service /etc/systemd/system/api.service 
 sudo cp celery.service /etc/systemd/system/celery.service
 
+export NG_CLI_ANALYTICS=ci
+
 cd app-ui && npm install
 ng --prod build
 cp -r dist/app-ui/* /var/www/html
 cd ..
 
+docker run --name redis --restart always -p 6379:6379 -d redis
 
 sudo mv /etc/nginx/sites-available/default /etc/nginx/default.bck 
 sudo cp nginx-no-ssl.conf /etc/nginx/sites-available/default
